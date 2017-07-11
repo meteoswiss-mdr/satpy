11e46118df588e93180812c7f251a66c14a1a66d
sam <marco.sassi@meteoswiss.ch>
2017-07-11 14:26:29 +0000

    add all changes by Marco Sassi (sam)

--- satpy/readers/nc_nwcsaf_msg.py
+++ satpy/readers/nc_nwcsaf_msg.py
@@ -66,18 +66,18 @@ class NcNWCSAFMSG(BaseFileHandler):
                                         variable.attrs['_FillValue'], copy=False)
         except KeyError:
             values = np.ma.array(variable[:], copy=False)
+
         if 'scale_factor' in variable.attrs:
             values = values * variable.attrs['scale_factor']
             info['scale_factor'] = variable.attrs['scale_factor']
+
         if 'add_offset' in variable.attrs:
             values = values + variable.attrs['add_offset']
             info['add_offset'] = variable.attrs['add_offset']
 
-#        info = {'platform_name': self.platform_name,
-#                'sensor': self.sensor}
-
         if 'valid_range' in variable.attrs:
             info['valid_range'] = variable.attrs['valid_range']
+
         if 'units' in variable.attrs:
             info['units'] = variable.attrs['units']
 
